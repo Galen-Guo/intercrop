44b1e156b60b0f5a63f5adc797e99e438d1a6d22
Galen-Guo <gguo094@uottawa.ca>
2022-05-19 07:10:56 -0400

    fix some typo, added maxbin2

--- 1718/NS_1718-6_anvio_cluster6.sh
+++ 1718/NS_1718-6_anvio_cluster6.sh
@@ -3,7 +3,7 @@
 #$ -m eas -M galen.guo@canada.ca
 #$ -o /isilon/ottawa-rdc/users/shared/chenw_lab/galen/temp_files
 #$ -e /isilon/ottawa-rdc/users/shared/chenw_lab/galen/temp_files
-#$ -N N18_profile
+#$ -N N18_clust6
 
 
 source ~/miniconda3/bin/activate
@@ -27,6 +27,8 @@ ANVIO=$WORK/anvio
 anvi-cluster-contigs -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -C plot6_concoct --driver concoct -T 24 --just-do-it
 anvi-cluster-contigs -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -C plot6_metabat2 --driver metabat2 -T 24 --just-do-it
 conda activate anvio-7.1
-anvi-cluster-contigs -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -S plot6_concoct,plot6_metabat2 --driver dastool --search-engine diamond -C plot6_dastool -T 24 --just-do-it
+anvi-cluster-contigs -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -C plot6_maxbin2 --driver maxbin2 -T 24 --just-do-it
+
+anvi-cluster-contigs -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -S plot6_concoct,plot6_metabat2,plot6_maxbin2 --driver dastool --search-engine diamond -C plot6_dastool -T 24 --just-do-it
 conda activate anvio7
 anvi-summarize -c -c $ANVIO/1718/plot6/plot6_1718_contigs.db -p $ANVIO/1718/plot6/profile_merged/PROFILE.db -o $ANVIO/1718/plot6/sample_summary_dastool -C plot6_dastool --init-gene-coverages
