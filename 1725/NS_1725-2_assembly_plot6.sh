0f831d83f7dcef4262a9c72f28106e20f1c6b42b
Galen-Guo <gguo094@uottawa.ca>
2022-05-19 08:23:53 -0400

    typo

--- 1725/NS_1725-2_assembly_plot6.sh
+++ 1725/NS_1725-2_assembly_plot6.sh
@@ -23,21 +23,21 @@ ANVIO=$WORK/anvio
 ### co-assembly megahit
 ###
 ####################################################################
-# for i in `awk '{print $1}' $WORK/NS.1718.plot34`;
+# for i in `awk '{print $1}' $WORK/NS.1725.plot34`;
 # do
 # echo $i
 # mv ${i}.pair1.fq.gz $TRIM/plot4
 # mv ${i}.pair2.fq.gz $TRIM/plot4
 # done
 #
-# for i in `awk '{print $1}' $WORK/NS.1718.plot5`;
+# for i in `awk '{print $1}' $WORK/NS.1725.plot5`;
 # do
 # echo $i
 # mv ${i}.pair1.fq.gz $TRIM/plot5
 # mv ${i}.pair2.fq.gz $TRIM/plot5
 # done
 #
-# for i in `awk '{print $1}' $WORK/NS.1718.plot6`;
+# for i in `awk '{print $1}' $WORK/NS.1725.plot6`;
 # do
 # echo $i
 # mv ${i}.pair1.fq.gz $TRIM/plot6
@@ -45,9 +45,9 @@ ANVIO=$WORK/anvio
 # done
 
 
-echo co-assembling 1718, by sequencing length
+echo co-assembling 1725, by sequencing length
 
-F=$(echo $(ls $TRIM/plot6/NS.1718.F8P*.pair1.fq.gz)  | sed "s/ /,/g")
-R=$(echo $(ls $TRIM/plot6/NS.1718.F8P*.pair2.fq.gz)  | sed "s/ /,/g")
+F=$(echo $(ls $TRIM/plot6/NS.1725.F8P*.pair1.fq.gz)  | sed "s/ /,/g")
+R=$(echo $(ls $TRIM/plot6/NS.1725.F8P*.pair2.fq.gz)  | sed "s/ /,/g")
 
-megahit -1 $F -2 $R -o $MEGAHIT/1718/plot6/ -t 12 --min-contig-len 1000 --continue  --k-min 37 --k-max 77 --k-step 10
+megahit -1 $F -2 $R -o $MEGAHIT/1725/plot6/ -t 12 --min-contig-len 1000 --continue  --k-min 37 --k-max 77 --k-step 10
